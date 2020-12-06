f9fbbe69ae5812d40e1d435ca09a446e760cefcb
GuyInGrey <guyingreydaboss@gmail.com>
2020-12-06 09:34:40 -0700

    Readme generation improvement

--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -36,6 +37,8 @@ namespace GuyInGrey_AoC2020
 
             while (true)
             {
+                var results = new List<List<TimingResult>>();
+
                 Console.Write("Which type to benchmark? (\"all\" for all): ");
                 var input = Console.ReadLine();
 
@@ -44,7 +47,7 @@ namespace GuyInGrey_AoC2020
                     var types = Assembly.GetExecutingAssembly().GetTypes().Where(
                         t => !(t.GetAttribute<PuzzleAttribute>() is null));
 
-                    BenchmarkTypes(types.ToArray());
+                    results = BenchmarkTypes(types.ToArray());
                 }
                 else
                 {
@@ -53,16 +56,47 @@ namespace GuyInGrey_AoC2020
                     {
                         Console.WriteLine("Invalid.");
                     }
-                    else { BenchmarkTypes(new[] { type }); }
+                    else { results = BenchmarkTypes(new[] { type }); }
+                }
+
+                Console.Write("Save results to readme? (y/n): ");
+                var answer = Console.ReadKey();
+                Console.WriteLine();
+
+                if (answer.Key == ConsoleKey.Y)
+                {
+                    var methodNames = results.Select(s => s.Select(s2 => s2.BenchmarkedMethod.Name))
+                        .SelectMany(s => s).ToList().Distinct();
+
+                    var markdown = "# Advent Of Code 2020\nGuyInGrey's Solutions!\n" +
+                        "Featuring a custom-made benchmarking system, accurate to 0.4 μs.\n\n## Timing Results:\n" +
+                        "|Name|" + string.Join("|", methodNames) + "|\n|-|-|\n";
+
+                    foreach (var line in results)
+                    {
+                        var lineOut = new List<string>();
+                        for (var i = 0; i < methodNames.Count(); i++)
+                        {
+                            lineOut.Add(" ");
+                        }
+
+                        for (var i = 0; i < line.Count; i++)
+                        {
+                            lineOut[methodNames.ToList().IndexOf(line[i].BenchmarkedMethod.Name)] = 
+                                line[i].TimeTaken.TotalMilliseconds + " ms";
+                        }
+                        markdown += $"|{line[0].Information.Name}|{string.Join("|", lineOut)}|\n";
+                    }
+                    File.WriteAllText(@"C:\Users\NoahL\source\repos\AdventOfCode2020\README.md", markdown);
                 }
             }
         }
 
-        public static void BenchmarkTypes(Type[] types)
+        public static List<List<TimingResult>> BenchmarkTypes(Type[] types)
         {
-            var markdown = "# Advent Of Code 2020\nGuyInGrey's Solutions!\n" +
-                "Featuring a custom-made benchmarking system, accurate to 0.4 μs.\n\n## Timing Results:\n" +
-                "|Name|Time|\n|-|-|\n";
+            
+
+            var toReturn = new List<List<TimingResult>>();
 
             Console.WriteLine(" - 1000 iterations each -");
             foreach (var t in types)
@@ -70,17 +104,19 @@ namespace GuyInGrey_AoC2020
                 var benchmark = new BenchmarkedPuzzle(t);
                 benchmark.Run(1000);
 
-                foreach (var r in benchmark.BenchmarkResults)
-                {
-                    markdown += $"|{r.Information.Name} - {r.BenchmarkedMethod.Name}|" +
-                        $"{r.TimeTaken.TotalMilliseconds} ms|\n";
-                }
-                markdown += "| | |\n";
+                //foreach (var r in benchmark.BenchmarkResults)
+                //{
+                //    markdown += $"|{r.Information.Name} - {r.BenchmarkedMethod.Name}|" +
+                //        $"{r.TimeTaken.TotalMilliseconds} ms|\n";
+                //}
+                //markdown += "| | |\n";
 
                 Console.WriteLine(string.Join("\n", benchmark.BenchmarkResults) + "\n");
+                toReturn.Add(benchmark.BenchmarkResults);
             }
 
-            File.WriteAllText(@"C:\Users\NoahL\source\repos\AdventOfCode2020\README.md", markdown);
+            return toReturn;
+            //File.WriteAllText(@"C:\Users\NoahL\source\repos\AdventOfCode2020\README.md", markdown);
         }
     }
 }
--- README.md
+++ README.md
@@ -3,29 +3,11 @@ GuyInGrey's Solutions!
 Featuring a custom-made benchmarking system, accurate to 0.4 μs.
 
 ## Timing Results:
-|Name|Time|
+|Name|Setup|Part1|Part2|
 |-|-|
-|Day1 - Setup|0.1276 ms|
-|Day1 - Part1|0.0343 ms|
-|Day1 - Part2|0.725 ms|
-| | |
-|Day2 - Setup|2.8139 ms|
-|Day2 - Part1|2.5064 ms|
-|Day2 - Part2|0.3466 ms|
-| | |
-|Day3 - Setup|0.263 ms|
-|Day3 - Part1|0.023 ms|
-|Day3 - Part2|0.0851 ms|
-| | |
-|Day4 - Setup|0.3438 ms|
-|Day4 - Part1|0.6748 ms|
-|Day4 - Part2|1.4518 ms|
-| | |
-|Day5 - Setup|0.5598 ms|
-|Day5 - Part1|0.0059 ms|
-|Day5 - Part2|0.0531 ms|
-| | |
-|Day6 - Setup|0.3677 ms|
-|Day6 - Part1|0.671 ms|
-|Day6 - Part2|1.2593 ms|
-| | |
+|Day1|0.1265 ms|0.0356 ms|0.7187 ms|
+|Day2|2.7503 ms|2.4104 ms|0.3346 ms|
+|Day3|0.2584 ms|0.0244 ms|0.0854 ms|
+|Day4|0.3413 ms|0.6705 ms|1.4411 ms|
+|Day5|0.542 ms|0.0058 ms|0.0526 ms|
+|Day6|0.3611 ms|0.6692 ms|1.2233 ms|
